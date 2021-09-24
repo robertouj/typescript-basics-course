d2453a9ddcc3aa04c0da4f3e4c33d76d9ecb2be3
Roberto Ubieto Juan <robertouj@gmail.com>
2021-09-24 11:56:23 +0200

    change strings syntax

--- src/01-types-assigns-inferrences.ts
+++ src/01-types-assigns-inferrences.ts
@@ -5,7 +5,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // }
   const result = n1 + n2;
   if (showResult) {
-    console.log(phrase + result);
+    console.log(`${phrase} ${result}`);
   }
 }
 
