1669053a0d4c4117807312b6648fed5fe0937766
Roberto Ubieto Juan <robertouj@gmail.com>
2021-10-05 13:02:59 +0200

    Example 16: Utility types

---
+++ src/16-utility-types.ts
@@ -0,0 +1,28 @@
+/**
+ * Utility Types:
+ * 
+ * - Partial, Readonly...
+ * 
+ * More: https://www.typescriptlang.org/docs/handbook/utility-types.html
+ */
+ interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {}; // it's empty and doesn't complain
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
+// names.push('Manu');
+// names.pop();
